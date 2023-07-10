b1dc76be4858fa7fc198023fa7691d48590ebd15
Josiane Magalhães <josymagalhaes18@gmail.com>
2023-07-10 16:17:04 -0300

    Add files via upload

---
+++ perfil/utills.py
@@ -0,0 +1,43 @@
+from extrato.models import Valores
+from contas.models import ContaPaga, ContaPagar
+from datetime import datetime
+def calcula_total(obj, campo):
+    total = 0
+    for i in obj:
+        total += getattr(i, campo)
+
+    return total
+
+def calcula_equilibrio_financeiro():
+    gastos_essenciais = Valores.objects.filter(data__month=datetime.now().month).filter(tipo='S').filter(categoria__essencial=True)
+    gastos_nao_essenciais = Valores.objects.filter(data__month=datetime.now().month).filter(tipo='S').filter(categoria__essencial=False)
+
+    total_gastos_essenciais = calcula_total(gastos_essenciais, 'valor')
+    total_gastos_nao_essenciais = calcula_total(gastos_nao_essenciais, 'valor')
+
+    total = total_gastos_essenciais + total_gastos_nao_essenciais
+    try:
+        percentual_gastos_essenciais = total_gastos_essenciais * 100 / total
+        percentual_gastos_nao_essenciais = total_gastos_nao_essenciais * 100 / total
+
+        return percentual_gastos_essenciais, percentual_gastos_nao_essenciais
+    except:
+        return 0, 0
+    
+def total_conta_vencida_e_proxima():
+    MES_ATUAL = datetime.now().month
+    DIA_ATUAL = datetime.now().day
+    
+    contas = ContaPagar.objects.all()
+
+    contas_pagas = ContaPaga.objects.filter(data_pagamento__month=MES_ATUAL).values('conta')
+
+    contas_vencidas = contas.filter(dia_pagamento__lt=DIA_ATUAL).exclude(id__in=contas_pagas)
+    
+    contas_proximas_vencimento = contas.filter(dia_pagamento__lte = DIA_ATUAL + 5).filter(dia_pagamento__gte=DIA_ATUAL).exclude(id__in=contas_pagas)
+    contas_vencidas_total = calcula_total(contas_vencidas, 'conta')
+    contas_proximas_vencimento_total = calcula_total(contas_proximas_vencimento, 'conta')
+
+    return contas_vencidas_total, contas_proximas_vencimento_total
+    
+    
